8fb3bad00e14cf1f095f9bd22eb14823dc95a0b3
karthirajan <kajekarthi@gmail.com>
2019-05-06 11:44:39 +0530

    again

--- src/org/sample/Test.java
+++ src/org/sample/Test.java
@@ -1,5 +1,15 @@
 package org.sample;
 
 public class Test {
+	
+	private void hi() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void welcome() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
